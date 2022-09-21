462017730e489b666cbb6aa8ae38c5403c7e0bd8
Fernando Andrade <fernandow.and@gmail.com>
2022-09-21 08:18:19 -0300

    Aula 06 - HTTP, StatusCode e Controller

---
+++ Consultorio/Controllers/AgendamentoController.cs
@@ -0,0 +1,40 @@
+﻿using Consultorio.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Consultorio.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AgendamentoController : ControllerBase
+    {
+        List<Agendamento> agendamentos = new List<Agendamento>();
+        public AgendamentoController()
+        {
+            agendamentos.Add(new Agendamento 
+            { 
+                Id = 1, 
+                NomePaciente = "Fernando", 
+                Horario = new DateTime(2022, 09, 21) 
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(agendamentos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetByID(int id)
+        {
+            var agendamentoSelecionado = agendamentos.FirstOrDefault(x => x.Id == id);
+
+            return agendamentoSelecionado != null 
+                ? Ok(agendamentos)
+                : BadRequest("Erro ao buscar o agendamento");
+        }
+    }
+}
