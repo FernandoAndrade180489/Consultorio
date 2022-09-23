5299b192b2ff0affc416a2fb62718cc21130c861
Fernando Andrade <fernandow.and@gmail.com>
2022-09-23 08:51:56 -0300

    Aula 21 - CRUD completo Especialidades - Post e Update Status

--- Consultorio/Controllers/EspecialidadeController.cs
+++ Consultorio/Controllers/EspecialidadeController.cs
@@ -43,7 +43,43 @@ namespace Consultorio.Controllers
             return especialidadeRetorno != null
                 ? Ok(especialidadeRetorno)
                 : NotFound("Nenhuma especialidade encontrada");
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> Post(EspecialidadeAdicionarDto especialidade)
+        {
+            if (string.IsNullOrEmpty(especialidade.Nome)) return BadRequest("Dados inválidos");
+
+            var especialidadeAdicionar = new Especialidade { 
+                Nome = especialidade.Nome, Ativa = especialidade.Ativa 
+            };
+
+            _repository.Add(especialidadeAdicionar);
+
+            return await _repository.SaveChangesAsync()
+                ? Ok(especialidade)
+                : BadRequest("Erro ao adicionar especialidade");
+        }
+
+        [HttpPut("{id:int}/atualizar-status/")]
+        public async Task<IActionResult> Put(int id, bool ativo)
+        {
+            if (id <= 0) return BadRequest("Especialidade inválida");
+
+            var especialidade = await _repository.GetEspecialidadeById(id);
+
+            if (especialidade == null) return NotFound("Especialidade não encontrada");
+
+            string status = ativo ? "ativa" : "inativa";
+            if (especialidade.Ativa == ativo) return Ok("A especialidade já está " + status);
+
+            especialidade.Ativa = ativo;
+
+            _repository.Update(especialidade);
+
+            return await _repository.SaveChangesAsync()
+                ? Ok("Status da especialidade atualizada")
+                : BadRequest("Erro ao atualizar status");
         }
 
     }
