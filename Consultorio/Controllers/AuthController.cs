495a19b9c6d1a00404ef8680cf5ce5836ec47261
Fernando Andrade <fernandow.and@gmail.com>
2022-09-26 15:45:35 -0300

    Criado AuthController para cadastrar nova conta e logar

---
+++ Consultorio/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using Consultorio.Models.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Consultorio.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        [HttpPost("nova-conta")]
+        public async Task<IActionResult> Registrar(RegisterUserViewModel registerUser)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true,
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return Ok(result);
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPost("entrar")]
+        public async Task<IActionResult> Login(LoginUserViewModel loginUser)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, isPersistent: false, lockoutOnFailure: true);
+
+            if (result.Succeeded) return Ok("Usuário Logado");
+
+            if (result.IsLockedOut) return BadRequest("Usuário temporariamente bloqueado.");
+
+            return BadRequest("Usuário ou Senha incorretos");
+        }
+    }
+}
---
+++ Consultorio/Models/Dtos/UserDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Consultorio.Models.Dtos
+{
+    public class RegisterUserViewModel
+    {
+        [Required(ErrorMessage = "O campo é obrigatório")]
+        [EmailAddress(ErrorMessage = "Formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo é obrigatório")]
+        [StringLength(50, ErrorMessage = "O campo precisa ter entre 6 e 50 carecteres", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "As senhas não conferem.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginUserViewModel
+    {
+        [Required(ErrorMessage = "O campo é obrigatório")]
+        [EmailAddress(ErrorMessage = "Formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo é obrigatório")]
+        [StringLength(50, ErrorMessage = "O campo precisa ter entre 6 e 50 carecteres", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
